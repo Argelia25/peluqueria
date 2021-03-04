61dbbec76442d0d6b7aca411920d5b5cb5b8eae3
Luz pérez <aregelianajera23@gmail.com>
2021-03-03 22:43:44 -0600

    se crea el sidebar donde colocamos los links para navegar en la pagina

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,76 @@
+  
+  <div class=" sidebar" role="navigation">
+    <div class="navbar-collapse">
+        <nav style="background-color:#361a3d;" class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+          <ul class="nav" id="side-menu">
+            <li>
+              <a  href="panel-control.php"><i class="fa fa-home nav_icon"></i>Panel de Control</a>
+            </li>
+            <li>
+              <a   href="agregar-servicio.php"><i class="fa fa-cogs nav_icon"></i>Servicios<span class="fa arrow"></span> </a>
+              <ul class="nav nav-second-level collapse">
+                <li>
+                  <a  href="agregar-servicio.php">Agregar Servicio</a>
+                </li>
+                <li>
+                  <a href="administrar-servicios.php">Administrar Servicio</a>
+                </li>
+              </ul>
+              <!-- /nav-second-level -->
+            </li>
+            <li class="">
+              <a  href="panel-control.php"><i class="fa fa-book nav_icon"></i>Páginas <span class="fa arrow"></span></a>
+              <ul class="nav nav-second-level collapse">
+                
+                <li>
+                  <a href="contactos.php">Contacto</a>
+                </li>
+              </ul>
+              <!-- /nav-segundo-nivel -->
+            </li>
+          
+            <li>
+              <a  href="total-citas.php"><i class="fa fa-check-square-o nav_icon"></i>Citas<span class="fa arrow"></span></a>
+              <ul class="nav nav-second-level collapse">
+                <li>
+                  <a  href="total-citas.php">Todas las Citas</a>
+                </li>
+                <li>
+                  <a  href="nueva-cita.php">Aceptar Cita</a>
+                </li>
+                <li>
+                  <a href="cita-aceptada.php">Citas Aceptadas</a>
+                </li>
+                <li>
+                  <a  href="cita-rechazada.php">Citas Rechazadas</a>
+                </li>
+              </ul>
+              <!-- //nav-segundo-nivel -->
+            </li>
+           
+        
+            <li>
+              <a href="agregar-cliente.php" class="chart-nav"><i class="fa fa-user nav_icon"></i>Agregar Cliente</a>
+            </li>
+             <li>
+              <a  href="lista-clientes.php" class="chart-nav"><i class="fa fa-users nav_icon"></i>Lista de Clientes</a>
+            </li>
+              <li>
+              <a  href="#"><i class="fa fa-check-square-o nav_icon"></i>Reportes<span class="fa arrow"></span></a>
+              <ul class="nav nav-second-level collapse">
+                 <li><a  href="facturas-fechas.php"> Facturas por Fecha</a></li>
+                   
+                    <li><a  href="reporte-ventas.php">Reporte de Ventas</a></li>
+              </ul>
+              <!-- //nav-second-level -->
+            </li>
+
+             <li>
+              <a href="lista-facturas.php" class="chart-nav"><i class="fa fa-file-text-o nav_icon"></i>Facturas</a>
+            </li>
+          
+          </ul>
+  
+        </nav>
+      </div>
+    </div>
