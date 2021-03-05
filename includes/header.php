24d4e158550644984c5328d1c0ce0d6ab092e217
Luz pérez <aregelianajera23@gmail.com>
2021-03-05 16:14:10 -0600

    se crea la parte del header

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+
+<nav  class="navbar navbar-light navbar-expand-lg " style="background-color:#c6b1c9; position:fixed; width:100% ; z-index:100;" >
+      <div  class="container">
+      <!--manipulacion del nombre de pagina principal --> 
+        
+        <a  style="color:black;  font-family:Times New Roman, Times, serif; " href="index.php">Salón de belleza, Peluqueria  y Spa </a>
+       
+        <div class="collapse navbar-collapse">
+          <ul  class="navbar-nav ml-auto">
+             <!--parte del navbar de las vistas de acceso al cliente--> 
+            
+            <li  class="nav-item "><a   style="color:black;"href="index.php" class="nav-link" >Inicio</a></li>
+            <li  class="nav-item"><a   style="color:black;" href="services.php" class="nav-link"  >Servicios</a></li>
+            <li  class="nav-item"><a style="color:black;" href="contact.php" class="nav-link" >Contacto</a></li>
+            <!--nos lleva a la vista principal del administrador -->
+            <li class="nav-item"><a style="color:black;" href="admin/index.php" class="nav-link">Admin</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
