a56c395b176817e3370f97f825d211a6a6be214f
Luz pérez <aregelianajera23@gmail.com>
2021-03-05 16:13:06 -0600

    se crea la parte de del footer

---
+++ includes/footer.php
@@ -0,0 +1,38 @@
+
+ <footer  style="background-color:#c6b1c9; color:black">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2 logo">QUIENES SOMOS</h2>
+                <P>Somos un centro dirigido a ambos sexos, donde nuestro objetivo principal es lograr que las personas
+                   se sientan mejor consigo mismas, mediante  nuestros servicios de  cuidado facial y  corporal, para mantener 
+                   y/o mejorar la apariencia física.</P>
+            </div>
+          </div>
+         
+          <div class="col-md" >
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2  logo text-center">MISION</h2>
+              <P>Brindamos una atención holística real en un espacio que brinda un escape único 
+                y tranquilo de las cargas de la visa cotidiana, cultivando la conexión entre el cuerpo, mente y tierra.
+              </P>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2M logo text-center">VISION</h2>
+                <P>Ser un líder en mejor salud y el bienestar de nuestra comunidad al proporcionar tratamientos inspirados
+                   en enseñar a nuestros clientes prácticas saludables y fomentar la educación sobre el bienestar.</P>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p>Certificacion Fronten y Backend <h6>"Luz Perez"</h6> 
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
