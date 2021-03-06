6a426e914c2696c50ea1d5cc3013ee53cf7ecbcd
Luz pérez <aregelianajera23@gmail.com>
2021-03-06 16:53:11 -0600

    se crea un documento para el footer para usarlo por medio de un iclude  a cualquiera de las vistas donde sera necesario

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<!--footer-->
+    <div style="background-color:#a18594 ;"  class="footer">
+       <p style="color:black ;" >Certificacion de Frontend y Baquend</p>
+    </div>
+        <!--//footer-->
